397482da065e2a59b00e9033ab48485ddf1d59c6
Alex2Pena <attn.Alex.P@gmail.com>
2020-03-18 13:16:09 -0700

    Completed lab 7

--- server.js
+++ server.js
@@ -14,47 +14,77 @@ const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+const superagent = require('superagent');
+
 
 app.get('/location', (request, response) => {
   // this is the city that the front end is sending us in the qurey
   // the query lives in the url after the ? htt://cooldomain.com?city=seattle
-  try{
-    let city = request.query.city;
-    console.log('😎', city);
-    let geo = require('./data/geo.json');
-    let location = new Location(geo[0], city);
-    response.send(location);
-  }
-  catch(err){
-    console.error(err);
-  }
+  let city = request.query.city;
+  console.log('😎', city);
+  let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
+  superagent.get(url)
+    .then(results => {
+      // let geoData = results.body;
+      let location = new Location(results.body[0], city);
+      response.status(200).send(location);
+    }).catch(err => console.error(err));
 });
 
 app.get('/weather', (request, response) => {
   let weather = [];
-  let day = require('./data/darksky.json');
-  day.daily.data.forEach(forecast => {
-    weather.push(new Weather(forecast));
-  });
-  response.status(200).json(weather);
+  let lat = request.query.latitude;
+  let lon = request.query.longitude;
+  let url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${lon}`;
+  superagent.get(url)
+    .then(results => {
+      let wData = results.body.daily.data;
+      wData.map(day => {
+        let newDay = new Weather(day);
+        weather.push(newDay);
+      });
+      response.status(200).send(weather);
+    });
 });
 
-function Weather(obj){
-  this.forecast = obj.summary;
-  this.time = new Date(obj.time * 1000).toString().slice(0,15);
-}
+app.get('/trails', (request, response) => {
+  let {latitude,longitude} = request.query;
+  let url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${process.env.TRAILS_API_KEY}`;
+  superagent.get(url)
+    .then(results => {
+      const dataObj = results.body.trails.map(trail => new Trail(trail));
+      response.status(200).send(dataObj);
+    });
+});
 
 
+// Constructor Functions
 function Location(obj, city){
   this.search_query = city;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 }
+function Weather(obj){
+  this.forecast = obj.summary;
+  this.time = new Date(obj.time * 1000).toString().slice(0,15);
+}
+function Trail(obj){
+  this.name = obj.name;
+  this.location = obj.location;
+  this.length = obj.length;
+  this.stars = obj.stars;
+  this.star_votes = obj.starVotes;
+  this.summary = obj.summary;
+  this.trail_url = obj.url;
+  this.conditions = obj.conditionStatus;
+  this.condition_date = obj.conditionDate.slice(0,10);
+  this.condition_time = obj.conditionDate.slice(11,19);
+}
 
-// app.get('*', (request, response) => {
-//   response.status(404).send('there is nothing on this page');
-// });
+app.get('*', (request, response) => {
+  response.status(404).send('there is nothing on this page');
+});
 
 // turn on the server
 app.listen(PORT, () => {
